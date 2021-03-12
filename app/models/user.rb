9db32a7f7e6bfb8c17b4af84aaf0ed9a885d2f63
fumiyaaaaa <on88.iwai.fumiya@gmail.com>
2021-03-12 16:32:26 +0900

    テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, uniqueness: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: PASSWORD_REGEX }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
     validates :chinese_character_last_name
     validates :chinese_character_first_name
@@ -15,4 +14,7 @@ class User < ApplicationRecord
     validates :kana_first_name
   end
   validates :birthday, presence: true
+
+  has_many :items
+  has_many :order_historys
 end
