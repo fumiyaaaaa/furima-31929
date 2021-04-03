95919f157281dd3d22ef284f396864df7c5b7061
fumiyaaaaa <on88.iwai.fumiya@gmail.com>
2021-04-03 14:36:10 +0900

    テーブル作成

---
+++ db/migrate/20210403014421_create_buyer_informations.rb
@@ -0,0 +1,14 @@
+class CreateBuyerInformations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :buyer_informations do |t|
+      t.string     :postal_code,   null: false
+      t.integer    :address_id,    null: false
+      t.string     :municipality,  null: false
+      t.string     :block_number,  null: false
+      t.string     :building_name, null: false
+      t.string     :tel,           null: false
+      t.references :order_history, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
